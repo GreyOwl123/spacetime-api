0de2bdbfbfb6bf4aca9b71f7d890b227771f822b
Muhammed Ibrahim <muib13579@gmail.com>
2024-03-20 13:01:43 +0100

    <tests>: tested object/create route

--- src/tests/routeTest.js
+++ src/tests/routeTest.js
@@ -5,10 +5,21 @@ import express from "express";
 const app = express();
 
 
-request(app)
-   .get('/')
-   .expect('Content-Type', /json/)
-   .expect(200)
-   .end(function(err, res) {
-      if (err) throw err;
-   })
+ request(app)
+    .post("/star/create", {
+        json: true,
+        body: {
+            name: "test1",
+            type: "test1",
+            reference: "test1",
+            distance: "test1",
+            time: "test1",
+            location: "test1",
+            summary: "test1"
+            }
+        })
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end(function(err,res) {
+                if (err) throw err;
+    });
